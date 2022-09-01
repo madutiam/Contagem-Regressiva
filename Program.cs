b12a9a1faf530d48599c79add3532c73349a2129
Maria Eduarda Bedetti Amorim <e107ti@etec.sp;gov;br>
2022-09-01 19:31:17 -0300

    finalmente

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Preparar...");
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+Console.WriteLine("Já");
